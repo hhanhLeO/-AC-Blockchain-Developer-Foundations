3c5ddb105eb80dfe583cc77aa27f7b680e3cb0f7
hhanhLeO <lthhanh04@gmail.com>
2025-10-14 23:09:28 +0700

    complete bai2_3 in Remix IDE

---
+++ lessons/bai2_3/Welcome.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Welcome {
+    string public greeting;
+    address public owner;
+    constructor(string memory _greeting) {
+        greeting = _greeting;
+        owner = msg.sender;
+    }
+    function getGreeting() public view returns (string memory, address) {
+        return (greeting, owner);
+    }
+}
