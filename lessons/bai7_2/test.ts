0b5e134f49ae2a0f9e50305c95e578b16714818a
hhanhLeO <lthhanh04@gmail.com>
2025-10-19 23:29:31 +0700

    complete bai7_1 and bai7_2

--- lessons/bai7_2/test.ts
+++ lessons/bai7_2/test.ts
@@ -1,2 +1,5 @@
 // Không cần kiểm thử bằng TypeScript
 console.log("Test contract trực tiếp bằng hardhat.");
+
+// Địa chỉ contract: 0x776018264B209dCa0a70c413095f6FE00867B476
+// Link trên etherscan: https://sepolia.etherscan.io/address/0x776018264B209dCa0a70c413095f6FE00867B476#code
