01964494478b0561ecca3daa6e46721ec3cd6789
hhanhLeO <lthhanh04@gmail.com>
2025-10-19 22:33:04 +0700

    complete bai6_1

--- lessons/bai6_1/test.ts
+++ lessons/bai6_1/test.ts
@@ -4,14 +4,13 @@ async function main() {
   const provider = new ethers.JsonRpcProvider("https://eth-sepolia.public.blastapi.io");
 
   const abi = [
-    ""
+    "function balanceOf(address owner) view returns (uint256)"
   ];
-  const contractAddress = "0x41d0Ad4E6227062B59CA673B418Fc00E3A2d10Cf"; // Replace with your contract address
+  const contractAddress = "0x7b8264e70da23ca6729fd6b25899dd25a769798d";
   const contract = new ethers.Contract(contractAddress, abi, provider);
 
-  /**
-   * Get the current balance of deployer
-   */
+  const balance = await contract.balanceOf("0x6731239daeA04426ce328c56AE0a0927fF4799cb");
+  console.log("Balance: ", balance); // 1000000000000000000000000000n
 }
 
 main().catch(console.error);
